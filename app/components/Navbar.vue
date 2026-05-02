9fee12ba0b87c12569968118b1d956c4c5598b08
khalid-webdev <khaleduzzaman67@gmail.com>
2026-05-02 23:15:40 +0600

    initals commit and navbar

---
+++ app/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="shadow-md  py-4">
+  <div class="container flex items-center justify-between">
+    <div class="logo font-bold text-2xl">Webdev</div>
+    <nav>
+      <ul class="flex items-center justify-center gap-4">
+        <li><NuxtLink class="hover:text-sky-500 px-2 font-semibold" to="/">Home</NuxtLink></li>
+        <li><NuxtLink class="hover:text-sky-500 px-2 font-semibold" to="/about">About</NuxtLink></li>
+        <li><NuxtLink class="hover:text-sky-500 px-2 font-semibold" to="/services">Services</NuxtLink></li>
+        <li><NuxtLink class="hover:text-sky-500 px-2 font-semibold" to="/works">Works</NuxtLink></li>
+      </ul>
+    </nav>
+    <button @click="navigateTo('/contact')" class="bg-black rounded-[20px] text-gray-100 py-2 px-5 hover:text-sky-600">Contact Me</button>
+  </div>
+</div>
+</template>
