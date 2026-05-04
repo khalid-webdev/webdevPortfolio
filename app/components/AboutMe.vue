2b73ce54b0d3d24d1a9773bb245d83594fa784ed
khalid-webdev <khaleduzzaman67@gmail.com>
2026-05-04 22:33:23 +0600

    updating about section

--- app/components/AboutMe.vue
+++ app/components/AboutMe.vue
@@ -10,7 +10,7 @@
         <p class="w-[80%] items-end text-gray-500">I am a passionate web developer with 3 years of experience in creating custom websites and ecommerce websites fully responsive. My goal is to help businesses and organizations achieve their goals through effective and efficient web development solutions. <br> <br> <br>As a dedicated web developer, I am committed to staying up-to date with my skills like nuxtjs, vuejs, nodejs, mongodb etc and trends in the industry. I approach each project with enthusiasm, creativity and a focus on delivering a high-quality end product that meets the unique needs of each client. With a passion for innovation and an eye for details, I specialize in creating intuitive and user-friendly web experiences. My goal is to not only meet but exceed my client's expectations and deliver exceptional results every time</p>
         <div class="flex justify-between mx-auto gap-16 mt-8 items-start">
           <div>
-            <h2 class="text-3xl mt-5 mb-3 font-semibold font-[Oswald]">Strategy</h2>
+            <h2 class="text-2xl mt-5 mb-3 font-semibold font-[Oswald]">Strategy</h2>
           <ul class="list-disc ml-8">
             <li>Brand Building</li>
             <li>Ecommerce website</li>
@@ -19,7 +19,7 @@
           </ul>
           </div>
           <div>
-            <h2 class="text-3xl mt-5 mb-3 font-semibold font-[Oswald]">My Skills</h2>
+            <h2 class="text-2xl mt-5 mb-3 font-semibold font-[Oswald]">My Skills</h2>
           <ul class="list-disc ml-8">
             <li>Nuxt</li>
             <li>Vue</li>
@@ -28,7 +28,7 @@
           </ul>
           </div>
           <div>
-            <h2 class="text-3xl mt-5 mb-3 font-semibold font-[Oswald]">Advice</h2>
+            <h2 class="text-2xl mt-5 mb-3 font-semibold font-[Oswald]">Advice</h2>
           <ul class="list-disc ml-8">
             <li>Change Management</li>
             <li>Process Management</li>
